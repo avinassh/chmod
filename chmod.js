4f66cefb721dcfb2024f930155edeae3ab09b21f
avi <avinashsajjan@gmail.com>
2013-09-26 19:49:30 +0530

    minor change

--- chmod.js
+++ chmod.js
@@ -1,6 +1,6 @@
 function check()
   {
-  	un_set();
+  	un_set('userTable');
   	var value=0;
   	tableData = document.getElementById('userTable').getElementsByTagName('td');
 	if (document.getElementById("uread").checked == true)
@@ -21,58 +21,64 @@ function check()
 			//document.getElementById('ue').innerHTML='x'
 			tableData[2].innerHTML='x';
 		}
-	document.getElementById('user').value=value;
+	//document.getElementById('user').value=value;
+	document.getElementById('chmodTable').getElementsByTagName('td')[0].getElementsByTagName('input')[0].value=value;
  }
 
-function set_ue(){
-	document.getElementById('ue').innerHTML='x';
+
+function set_ur(tableName){
+	document.getElementById(tableName).getElementsByTagName('td')[0].innerHTML='r';
+	//document.getElementById('ur').innerHTML='r';
 }
 
-function set_uw(){
-	document.getElementById('uw').innerHTML='w';
+function set_uw(tableName){
+	document.getElementById(tableName).getElementsByTagName('td')[1].innerHTML='w';
+	//document.getElementById('uw').innerHTML='w';
 }
 
-function set_ur(){
-	document.getElementById('ur').innerHTML='r';
+function set_ue(tableName){
+	document.getElementById(tableName).getElementsByTagName('td')[2].innerHTML='x';
+	//document.getElementById('ue').innerHTML='x';
 }
 
-function un_set(){
-	tableData = document.getElementById('userTable').getElementsByTagName('td');
-	//document.getElementById('ue').innerHTML='-';
-	//document.getElementById('uw').innerHTML='-';
-	//document.getElementById('ur').innerHTML='-';
+function un_set(tableName){
+	var tableData = document.getElementById(tableName).getElementsByTagName('td');
+	for (var i = tableData.length - 1; i >= 0; i--) {
+		tableData[i].innerHTML='-';
+	};
 }
 
-function check2 () {
- 	user_val = document.getElementById('user').value;
- 	un_set();
+function check2 (tableName) {
+ 	var user_val = document.getElementById('chmodTable').getElementsByTagName('td')[0].getElementsByTagName('input')[0].value;//document.getElementById('user').value;
+ 	un_set(tableName);
+ 	//var tableName = 'userTable';
  	if (user_val==1)
  		{
- 			set_ue();
+ 			set_ue(tableName);
  		}
  	if (user_val==2)
  		{
- 			set_uw();
+ 			set_uw(tableName);
  		}
  	if (user_val==3)
  		{ 
- 			set_uw(); set_ue();
+ 			set_uw(tableName); set_ue(tableName);
  		}
  	if (user_val==4)
  		{
- 			set_ur();
+ 			set_ur(tableName);
  		}
  	if (user_val==5)
  		{
- 			set_ur(); set_ue();
+ 			set_ur(tableName); set_ue(tableName);
  		}
  	if (user_val==6)
  		{
- 			set_ur();set_uw();
+ 			set_ur(tableName);set_uw(tableName);
  		}
  	if (user_val==7)
  		{
- 			set_ur();set_uw();set_ue();
+ 			set_ur(tableName);set_uw(tableName);set_ue(tableName);
  		}
  }
 
