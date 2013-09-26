7cc36ee3da18ac8c39ec6fb8a6ca4b2a0b4dbe3e
avi <avinashsajjan@gmail.com>
2013-09-26 19:50:46 +0530

    removed all comments

--- chmod.js
+++ chmod.js
@@ -6,39 +6,32 @@ function check()
 	if (document.getElementById("uread").checked == true)
 		{
 			value+=4;
-			//document.getElementById('ur').innerHTML='r'
 			tableData[0].innerHTML='r';
 		}	
 	if (document.getElementById("uwrite").checked == true)
 		{
 			value+=2;
-			//document.getElementById('uw').innerHTML='w';
 			tableData[1].innerHTML='w';
 		}
 	if (document.getElementById("uexec").checked == true)
 		{
 			value+=1;
-			//document.getElementById('ue').innerHTML='x'
 			tableData[2].innerHTML='x';
 		}
-	//document.getElementById('user').value=value;
 	document.getElementById('chmodTable').getElementsByTagName('td')[0].getElementsByTagName('input')[0].value=value;
  }
 
 
 function set_ur(tableName){
 	document.getElementById(tableName).getElementsByTagName('td')[0].innerHTML='r';
-	//document.getElementById('ur').innerHTML='r';
 }
 
 function set_uw(tableName){
 	document.getElementById(tableName).getElementsByTagName('td')[1].innerHTML='w';
-	//document.getElementById('uw').innerHTML='w';
 }
 
 function set_ue(tableName){
 	document.getElementById(tableName).getElementsByTagName('td')[2].innerHTML='x';
-	//document.getElementById('ue').innerHTML='x';
 }
 
 function un_set(tableName){
@@ -51,7 +44,6 @@ function un_set(tableName){
 function check2 (tableName) {
  	var user_val = document.getElementById('chmodTable').getElementsByTagName('td')[0].getElementsByTagName('input')[0].value;//document.getElementById('user').value;
  	un_set(tableName);
- 	//var tableName = 'userTable';
  	if (user_val==1)
  		{
  			set_ue(tableName);
